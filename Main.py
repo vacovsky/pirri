5181c4d247be3c68a805cbd010ea90e2b2c189c2
Joe Vacovsky <vacovsky@gmail.com>
2016-09-23 21:51:06 -0700

    Derp

--- Main.py
+++ Main.py
@@ -3,11 +3,12 @@ from ButtonControl import ButtonControl
 
 
 class Main:
-    relay_pin = 12
+    relay_pins = [18, 23, 24, 25, 12, 16, 20, 21, 4, 17, 27, 22, 5, 6, 13, 26]
 
     def __init__(self):
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(12, 0)
+        for pin in self.relay_pins:
+            GPIO.setup(pin, 0)
 
     def start(self):
         pass
--- WebApp.py
+++ WebApp.py
@@ -53,4 +53,5 @@ def send():
 
 
 if __name__ == '__main__':
+
     app.run(host='0.0.0.0', debug=True, port=config.PORT, threaded=True)
