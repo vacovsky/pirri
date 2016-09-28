1458f3ca72cb03881493df406789203d428672b8
Joe Vacovsky <vacovsky@gmail.com>
2016-09-28 15:08:16 -0700

    Html cleanup

--- scratch.py
+++ scratch.py
@@ -10,6 +10,13 @@ import setproctitle
 setproctitle.setproctitle("pirriweb")
 
 
-
 print(Process().name())
 
+
+from helpers.MessageHelper import RMQ
+
+RMQ().publish_message({
+    'sid': 52,
+    'duration': 10,
+    'schedule_id': 0
+})
