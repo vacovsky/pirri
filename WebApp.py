6bc62e9265df9b1f859245de772896e66a4924da
Joe Vacovsky <vacovsky@gmail.com>
2016-10-01 20:20:08 -0700

    Rwegr

--- WebApp.py
+++ WebApp.py
@@ -45,6 +45,22 @@ def schedule_edit():
     return jsonify(response)
 
 
+@app.route('/station/lastruns', methods=["GET"])
+def station_lastruns():
+    response = {
+        'lastrunlist': WebDataHelper.get_last_station_run()
+    }
+    return jsonify(response)
+
+
+@app.route('/station/nextruns', methods=["GET"])
+def station_nextruns():
+    response = {
+        'lastrunlist': WebDataHelper.get_next_station_run()
+    }
+    return jsonify(response)
+
+
 @app.route('/schedule/add', methods=["POST"])
 def schedule_add():
     data = json.loads(request.data.decode('utf8'))
